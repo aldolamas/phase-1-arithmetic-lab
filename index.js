7f26a9791236f6e70672080ee62e654dd9a6fede
Aldo Lamas <aldolamas01@gmail.com>
2022-03-25 01:45:51 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
+multiply;
 
-// Write your code here
+let random = Math.ceil(Math.random ())
+
+const num3 = 24
+const num4 = 5
+let mod = num3 % num4
+mod;
+
+let max = Math.max(5, 10, 15, 20)
