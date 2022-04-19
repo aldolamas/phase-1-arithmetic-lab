511306916ed80f456f00b08870df4e7398dd1961
Aldo Lamas <aldolamas01@gmail.com>
2022-04-19 17:53:24 -0500

    commit tree f63d53c8ef892387f46e3ea1e9bff93e34cde5c5

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const num1 = 31
 const num2 = 2
 let multiply = num1 * num2
-multiply;
 
-let random = Math.ceil(Math.random ())
+let random = Math.ceil(Math.random())
 
 const num3 = 24
 const num4 = 5
 let mod = num3 % num4
-mod;
 
 let max = Math.max(5, 10, 15, 20)
